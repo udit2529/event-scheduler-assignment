ceb564e9fb2da641059891881cc8bbf586bd460c
udit2529 <uditmaheshwari17@gmail.com>
2025-02-17 20:46:59 +0530

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight:"500" });
+
+export const metadata: Metadata = {
+  title: "Calendar App",
+  description: "By Solve It Out",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
